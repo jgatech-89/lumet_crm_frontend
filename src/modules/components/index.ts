b1a5889bc1b95a10dd4a33616659966ea09a2cda
Jose Ochoa <jochoacairoza492@gmail.com>
2026-04-08 16:32:59 -0500

    feat / componente generales de botones

---
+++ src/modules/components/index.ts
@@ -0,0 +1,6 @@
+export { CustomButton } from './buttons/buttons';
+export type { CustomButtonProps } from './buttons/buttons';
+export { ActionIconButton } from './buttons/actionIconButton';
+export type { ActionIconButtonProps } from './buttons/actionIconButton';
+export { BUTTON_PRESETS, getButtonPreset } from './buttons/buttonPresets';
+export type { ButtonPreset, ButtonPresetKey } from './buttons/buttonPresets';
